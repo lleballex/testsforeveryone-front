747982a2c5f7d5dba2e33757b8a23c26949a0952
lleballex <lleballex@yandex.ru>
2022-04-10 11:24:51 +0300

    Updated aspect ration of test image in update.vue

--- src/pages/tests/my/[id]/update.vue
+++ src/pages/tests/my/[id]/update.vue
@@ -35,7 +35,7 @@
       <icon icon="arrow-right" class="test__submit-icon" />
     </button>
 
-    <UiCropper ref="cropper" v-model="test.image" :aspectRatio="10/7" />
+    <UiCropper ref="cropper" v-model="test.image" :aspectRatio="4/3" />
   </div>
 </template>
 
@@ -97,7 +97,7 @@
     &:after {
       content: '';
       display: block;
-      padding-top: calc(7 / 10 * 100%);
+      padding-top: calc(3 / 4 * 100%);
     }
 
     > img {
